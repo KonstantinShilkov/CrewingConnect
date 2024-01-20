ec889f06e58cbce513b49b6e528ec5978ec2b1f4
KonstantinShilkov <shilkv1991@gmail.com>
2024-01-20 11:04:38 +0300

    HeaderNav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header/Header';
+import Contacts from './Components/Contacts';
+import Vacancies from './Components/Vacancies';
+import Login from './Components/Login';
+import Register from './Components/Register';
 
 function App() {
-  return (
-    <div >
-      
+  return ( 
+    <BrowserRouter>
+    <div className='container'>
+      <div className='app-wrapper'>
+      <Header  />
+      </div>
+      <div className='app-wrapper-content'>
+      <Routes>
+      <Route path="/" element={<Navigate to="/vacancies" replace />} />
+        <Route path="/vacancies" element={<Vacancies />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<div>404 NOT FOUND</div>} />
+      </Routes>
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HeaderNav from './HeaderNav';
+import s from './Header.module.css'
+
+
+
+const Header = () => {
+    return (
+        <div>
+            <HeaderNav/>
+         </div>
+    )
+}
+
+export default Header
